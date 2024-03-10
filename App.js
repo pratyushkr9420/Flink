bf5b301a19347cda1a7560d4a4cffd0418051a73
Pratyush Kumar <kumarpratyush6294@gmail.com>
2024-03-09 20:28:01 -0500

    added font files, updated Splashscreen, loaded all fonts

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useEffect, useCallback, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppisLoaded] = useState(false);
+
+  useEffect(() => {
+    const fontLoader = async () => {
+      try {
+        await Font.loadAsync({
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          black: require("./assets/fonts/Roboto-Black.ttf"),
+        });
+      } catch (error) {
+        console.log("Following error occured when loading fonts", error);
+      } finally {
+        setAppisLoaded(true);
+      }
+    };
+    fontLoader();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <SafeAreaView>
+        <Text style={{ fontSize: 30, fontFamily: "black" }}>
+          Welcome to Flink!
+        </Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
